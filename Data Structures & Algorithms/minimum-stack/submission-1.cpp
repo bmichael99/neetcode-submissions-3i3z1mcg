aac3ef1987832ede8c24b828312378f197e90259
Michael Brandmeyer <39311170+bmichael99@users.noreply.github.com>
2026-05-18 17:04:26 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,37 @@
+//have the stack keep track of its local min, so stack is a stack of pairs where each element in the stack keeps track of the min in relationship to itself and the numbers below it in the stack. That way the top of stack always shows the min value.
+class MinStack {
+public:
+    stack<pair<int,int>> st;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty()){
+          st.push({val,val});
+        } else {
+          st.push({val,min(val,st.top().second)});
+        }
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
