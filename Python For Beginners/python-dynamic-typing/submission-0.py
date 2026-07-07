369f68aa21c47e2a1b9acee3e1ad0acff40b8e56
Michael Brandmeyer <39311170+bmichael99@users.noreply.github.com>
2026-07-07 16:12:31 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,19 @@
+variable = 10
+
+print(type(variable))
+
+variable = 10.0
+
+print(type(variable))
+
+variable = True
+
+print(type(variable))
+
+variable = "false"
+
+print(type(variable))
+
+variable = []
+
+print(type(variable))
